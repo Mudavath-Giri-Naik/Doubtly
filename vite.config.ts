ccab945a7b117aa9dcb8e40f01cc5ba34b5f9d1e
Mudavath-Giri-Naik <mudavathgirinaik444@gmail.com>
2025-07-13 23:04:13 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// Add the static copy plugin
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.'
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
 })
