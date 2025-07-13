5d6db33315cbb3ea21a457071f7c842b4607d82b
Mudavath-Giri-Naik <mudavathgirinaik444@gmail.com>
2025-07-13 19:29:59 +0530

    added dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import ProfessorDashboard from './pages/ProfessorDashboard';
-import StudentJoinForm from './pages/StudentJoinForm';
-import ChatRoom from './pages/ChatRoom';
+import StudentJoinForm from './pages/StudentJoinForm.tsx';
+import ChatRoom from './pages/ChatRoom.tsx';
 
 function App() {
   return (
